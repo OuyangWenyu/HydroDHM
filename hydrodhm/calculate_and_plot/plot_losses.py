95968794d9af74fa0aea22b66adb49985b5b6e26
owen <wenyuouyang@outlook.com>
2024-09-24 11:53:50 +0800

    add more scripts and set leg for loss fig

--- hydrodhm/calculate_and_plot/plot_losses.py
+++ hydrodhm/calculate_and_plot/plot_losses.py
@@ -1,10 +1,10 @@
 """
 Author: Wenyu Ouyang
 Date: 2024-09-20 21:08:24
-LastEditTime: 2024-09-21 09:36:29
+LastEditTime: 2024-09-24 10:57:54
 LastEditors: Wenyu Ouyang
 Description: Plot the time series of losses for a basin
-FilePath: \HydroDHM\hydrodhm\check\plot_losses.py
+FilePath: \HydroDHM\hydrodhm\calculate_and_plot\plot_losses.py
 Copyright (c) 2023-2024 Wenyu Ouyang. All rights reserved.
 """
 
@@ -19,7 +19,7 @@ from definitions import CHANGDIAN_IDS, RESULT_DIR
 from hydrodhm.utils.results_plot_utils import plot_losses_ts
 
 
-def plot_losses_for_changdianbasin(basin_id):
+def plot_losses_for_changdianbasin(basin_id, leg_lst=None):
     changdian_basin_dpl_dir = os.path.join(
         RESULT_DIR,
         "dPL",
@@ -38,16 +38,11 @@ def plot_losses_for_changdianbasin(basin_id):
     )
     plot_losses_ts(
         [changdian_basin_dpl_dir, changdian_basin_dplnn_dir],
-        leg_lst=[
-            "dPL_calib.",
-            "dPL$_{\mathrm{nn}}$_calib.",
-            "dPL_val.",
-            "dPL$_{\mathrm{nn}}$_val.",
-        ],
+        leg_lst=leg_lst,
     )
 
 
-def plot_losses_for_reverse_changdianbasin(basin_id):
+def plot_losses_for_reverse_changdianbasin(basin_id, leg_lst=None):
     changdian_basin_dpl_dir = os.path.join(
         RESULT_DIR,
         "dPL",
@@ -66,16 +61,17 @@ def plot_losses_for_reverse_changdianbasin(basin_id):
     )
     plot_losses_ts(
         [changdian_basin_dpl_dir, changdian_basin_dplnn_dir],
-        leg_lst=[
-            "dPL_calib.",
-            "dPL$_{\mathrm{nn}}$_calib.",
-            "dPL_val.",
-            "dPL$_{\mathrm{nn}}$_val.",
-        ],
+        leg_lst=leg_lst,
     )
 
 
+leg_lst = [
+    "dPL_calib.",
+    "dPL$_{\mathrm{nn}}$_calib.",
+    "dPL_val.",
+    "dPL$_{\mathrm{nn}}$_val.",
+]
 for _basin_id in CHANGDIAN_IDS:
-    plot_losses_for_changdianbasin(_basin_id)
+    plot_losses_for_changdianbasin(_basin_id, leg_lst=leg_lst)
 for _basin_id in CHANGDIAN_IDS:
     plot_losses_for_reverse_changdianbasin(_basin_id)
